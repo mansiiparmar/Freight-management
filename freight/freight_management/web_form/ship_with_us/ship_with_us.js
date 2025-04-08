050179309b93af398ed79a9a428e851b59d4b34a
kumail29 <kumel306@gmail.com>
2025-04-08 15:45:40 +0530

    [commit-2 by kumel]

--- freight/freight_management/web_form/ship_with_us/ship_with_us.js
+++ freight/freight_management/web_form/ship_with_us/ship_with_us.js
@@ -2,23 +2,25 @@ frappe.ready(function() {
 	// bind events here
 })
 
-// frappe.ready(function() {
+frappe.ready(function() {
     
-//     // fetchDataFromOtherDocType();
-// });
+    fetchDataFromOtherDocType();
+});
 
-// function fetchDataFromOtherDocType() {
-//     frappe.call({
-//         method: "frappe.client.get_value",
-//         args: {
-//             doctype: "Customer", 
-//             fieldname: ["customer_name"],
-//             filters: { customer_name: "Mr kumel gandhi" } 
-//         },
-//         callback: function(response) {
-//             if (response.message) {
-//           frappe.web_form.set_value("first_name", response.message.customer_name);
-//             }
-//         }
-//     });
-// }
+function fetchDataFromOtherDocType() {
+    frappe.call({
+        method: "frappe.client.get_value",
+        args: {
+            doctype: "Customer", 
+            fieldname: ["customer_name"]
+            // filters: { customer_name: "Mr kumel gandhi" } 
+        },
+        callback: function(response) {
+            if (response.message) {
+          frappe.web_form.set_value("first_name", response.message.customer_name);
+
+                frappe.msgprint("/////");
+            }
+        }
+    });
+}
